dfec2d7c8f691af750a5dc6239f656eb31c85077
Vladimir Grigorev <escape13.96@mail.ru>
2023-11-09 15:50:09 +0400

    basic junit

---
+++ src/test/java/com/gmail/FirstSimpleTest.java
@@ -0,0 +1,38 @@
+package com.gmail;
+
+import org.junit.jupiter.api.*;
+
+@DisplayName("It is a magic")
+public class FirstSimpleTest {
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("@Before method");
+    }
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("        @BeforEach method");
+    }
+    @AfterEach
+    void afterEach(){
+        System.out.println("        @AfterEach method");
+    }
+    @AfterAll
+    static void afterAll(){
+        System.out.println("@AfterAll method");
+    }
+    @DisplayName("It is a test AssertTrue")
+    @Test
+    void firstTest(){
+        System.out.println("        It is a test AssertTrue");
+        Assertions.assertTrue(10>1);
+
+    }
+
+    @DisplayName("It is a test AssertEquals")
+    @Test
+    void SecondTest(){
+        System.out.println("        It is a test AssertEquals");
+        Assertions.assertEquals(10, 10);
+
+    }
+}
